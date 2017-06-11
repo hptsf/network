cfd4fd4a8a6d5702c210da3bbb54f32191110365
hptsf <hptsfb@gmail.com>
2017-06-11 00:44:46 -0400

    add comm dir; support options ops for sock

---
+++ comm/comm.h
@@ -0,0 +1,15 @@
+#ifndef __COMM_H__
+#define __COMM_H__
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+#endif
